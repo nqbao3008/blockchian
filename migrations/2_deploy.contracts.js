3e07a9f15435e51588c0963b4b920b52701085c6
nqbao3008 <nguyenquocbao3095@gmail.com>
2018-01-24 11:00:48 +0700

    First version

---
+++ migrations/2_deploy.contracts.js
@@ -0,0 +1,27 @@
+const IcoToken = artifacts.require('IcoToken');
+const IcoContract = artifacts.require('IcoContract');
+
+module.exports = function(deployer) {
+  deployer.deploy(
+    IcoToken,
+    'Test Token',
+    'TST',
+    '18',
+    '1.0'
+  ).then(() => {
+    return deployer.deploy(
+      IcoContract,
+      ' 0x7180bc53300de4765161ca42db00f735c5d7876c', // Your ETH Address
+      IcoToken.address,
+      '100000000000000000000000000', // 100000000 Token
+      '1000', // 1 ETH = 1000 Token
+      '1504051200', // 30/08/2017
+      '1576800000', // Friday 20th December 2019 07:00:00 AM
+      '100000000000000000' // 0.1 ETH
+    ).then(() => {
+      return IcoToken.deployed().then(function(instance) {
+        return instance.setIcoContract(IcoContract.address);
+      });
+    });
+  });
+};
